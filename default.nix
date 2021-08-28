01dcb03748e22c535278d31f9a77e69c1ddc52ec
Yana Timoshenko <yana@riseup.net>
2021-08-28 19:49:35 +0300

    Rename .#filth-ios to .#ask-lib-ios

--- default.nix
+++ default.nix
@@ -3,13 +3,17 @@
 with pkgs;
 
 let
+  # Documentation:
+  # https://input-output-hk.github.io/haskell.nix/reference/library/#cabalproject
   project = import ./nix/project.nix { inherit pkgs; };
 in
 
 {
-  ask = project.hsPkgs.ask.components.exes.ask;
+  ask =
+    project.hsPkgs.ask.components.exes.ask;
 
   # Only builds on macOS, see:
   # https://github.com/input-output-hk/haskell.nix/issues/1099
-  filth-ios = project.projectCross.iphone64.hsPkgs.ask.components.library;
+  ask-lib-ios =
+    project.projectCross.iphone64.hsPkgs.ask.components.library;
 }
